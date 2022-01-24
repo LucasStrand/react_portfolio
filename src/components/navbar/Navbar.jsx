0af499610786b0c2e5b1d6307c571d2890999b5a
LucasStrand <57836852+LucasStrand@users.noreply.github.com>
2022-01-24 15:02:53 +0100

    navbar looks ok

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,24 @@
 import './navbar.scss'
+import {Person, Mail} from '@material-ui/icons'
 
 export default function Navbar() {
   return (
       <div className='navbar'>
           <div className="wrapper">
               <div className="left">
-                  <a href="#introduction">Introduction</a>
-                  <a href="#portfolio">Portfolio</a>
-                  <a href="#works">Works</a>
-                  <a href="#contact">contact</a>
+                  <a href="#introduction" className='logo'>strand.</a>
+                  <div className="itemContainer">
+                      <Person className='icon' />
+                      <span>+46739906160</span>
+                  </div>
+                  <div className="itemContainer">
+                      <Mail className='icon'/>
+                      <span>lucas.strand@outlook.com</span>
+                  </div>
               </div>
-              <div className="right"></div>
+              <div className="right">
+                  
+              </div>
           </div>
       </div>
   )
