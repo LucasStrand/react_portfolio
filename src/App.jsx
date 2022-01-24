a63a665ee4ef033aea68fb1f54e507f96da8a2c4
LucasStrand <57836852+LucasStrand@users.noreply.github.com>
2022-01-24 10:02:05 +0100

    added components and sass

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from "./components/navbar/Navbar";
+import Introduction from './components/introduction/Introduction';
+import Portfolio from './components/portfolio/Portfolio';
+import Works from './components/works/Works';
+import Testimonials from './components/testimonials/Testimonials';
+import Contact from './components/contact/Contact';
+
+
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <div className="sections">
+        <Introduction />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
