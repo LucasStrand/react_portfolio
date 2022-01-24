1bda4f282972958904b9e53d64082e080456b7c5
LucasStrand <57836852+LucasStrand@users.noreply.github.com>
2022-01-24 15:28:45 +0100

    burgir animation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import './navbar.scss'
 import {Person, Mail} from '@material-ui/icons'
 
-export default function Navbar() {
+export default function Navbar({ menuOpen, setMenuOpen }) {
   return (
-      <div className='navbar'>
+      <div className={'navbar ' + (menuOpen && 'active')}>
           <div className="wrapper">
               <div className="left">
                   <a href="#introduction" className='logo'>strand.</a>
@@ -17,7 +17,11 @@ export default function Navbar() {
                   </div>
               </div>
               <div className="right">
-                  
+                  <div className="burgir" onClick={()=>setMenuOpen(!menuOpen)}>
+                      <span className='line1'></span>
+                      <span className='line2'></span>
+                      <span className='line3'></span>
+                  </div>
               </div>
           </div>
       </div>
