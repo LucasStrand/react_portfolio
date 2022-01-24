696672c1e0c61c9a2ccb65d51c9ed1269c7c5d2d
LucasStrand <57836852+LucasStrand@users.noreply.github.com>
2022-01-24 10:59:33 +0100

    added navbar items

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,15 @@ import './navbar.scss'
 export default function Navbar() {
   return (
       <div className='navbar'>
-          
+          <div className="wrapper">
+              <div className="left">
+                  <a href="#introduction">Introduction</a>
+                  <a href="#portfolio">Portfolio</a>
+                  <a href="#works">Works</a>
+                  <a href="#contact">contact</a>
+              </div>
+              <div className="right"></div>
+          </div>
       </div>
   )
 }
