40732eef11b51a80d56c9d52de38598eba1922f7
LucasStrand <57836852+LucasStrand@users.noreply.github.com>
2022-01-24 16:01:22 +0100

    sidemenu added

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
 import "./app.scss"
 import { useState } from "react";
+import Menu from "./components/menu/Menu";
 
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="app">
-      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Introduction />
         <Portfolio />
