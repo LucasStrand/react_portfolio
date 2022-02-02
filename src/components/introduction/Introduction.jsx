d40bc131716b3339af2c627bbed1a1f2381526a1
LucasStrand <57836852+LucasStrand@users.noreply.github.com>
2022-02-02 09:35:20 +0100

    introduction completeish

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -1,7 +1,20 @@
 import './introduction.scss'
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
 export default function Introduction({ menuOpen, setMenuOpen }) {
+
+  const textRef = useRef();
+
+  useEffect(()=>{
+      init(textRef.current, {
+        showCursor: true,
+        backDelay: 1500,
+        backSpeed: 60,
+        strings:['Full Stack Developer', 'Student']
+      })
+  },[])
   return (
       <div className='introduction' id="introduction">
         <div className="left">
@@ -13,7 +26,7 @@ export default function Introduction({ menuOpen, setMenuOpen }) {
           <div className="wrapper">
             <h2>Hello there, I'm</h2>
             <h1>Lucas Strand</h1>
-            <h3>Student <span>Developer</span></h3>
+            <h3>I'm a <span ref={textRef}></span></h3>
           </div>
           <a href="#portfolio">
             <img src="assets/thiccerDown.png" alt="" />
